b76af5a8fc825c42aa2c52fd18ca70abed3ba8d8
EliasXabs <eliasxabs@gmail.com>
2022-11-03 22:58:31 +0200

    even faster??

--- JavaSolutions/easy/problem_1.java
+++ JavaSolutions/easy/problem_1.java
@@ -16,16 +16,17 @@ public class problem_1 {
      */
 
     public int[] twoSum(int[] nums, int target) {
-        // 5ms solution
+        // 2ms solution
         // 45.5 MB
         Map<Integer, Integer> hash = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
+
             if (hash.containsKey(target - nums[i])) {
                 return new int[] { i, hash.get(target - nums[i]) };
-            } else {
-                hash.put(nums[i], i);
             }
+            hash.put(nums[i], i);
+
         }
         return new int[] { -1, -1 };
     }
