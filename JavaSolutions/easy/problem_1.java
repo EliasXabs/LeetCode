7b7fb51674c42fadae2d3346bb9c96f83ac3e243
EliasXabs <eliasxabs@gmail.com>
2022-11-03 20:08:10 +0200

    n1 (not optimal)

---
+++ JavaSolutions/easy/problem_1.java
@@ -0,0 +1,28 @@
+package JavaSolutions.easy;
+
+public class problem_1 {
+
+    /*
+     * Given an array of integers nums and an integer target, return indices of the
+     * two numbers such that they add up to target.
+     * 
+     * You may assume that each input would have exactly one solution, and you may
+     * not use the same element twice.
+     * 
+     * You can return the answer in any order.
+     */
+
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (j != i && (nums[i] + nums[j]) == target) {
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+        }
+        return null;
+    }
+}
